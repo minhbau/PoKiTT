8f315501b74a775d914fc3124babfed69da87d89
Nathan Yonkee <nathan.yonkee@chemeng.utah.edu>
2014-08-15 10:22:02 -0600

    Change HeatCapacity_Cp to calculate mix cp

--- pokitt/thermo/HeatCapacity_Cp.h
+++ pokitt/thermo/HeatCapacity_Cp.h
@@ -10,39 +10,45 @@
 
 #include <pokitt/CanteraObjects.h> //include cantera wrapper
 
-#include <cantera/kernel/ct_defs.h> // contains value of gas constant
+#include <cantera/kernel/ct_defs.h> // contains value of Cantera::GasConstant
 #include <cantera/kernel/speciesThermoTypes.h> // contains definitions for which polynomial is being used
 #include <cantera/IdealGasMix.h>
 
 namespace Cantera_CXX{ class IdealGasMix; } // location of polynomial coefficients
 
 /**
- *  \class  HeatCapacity_Cp
+ *  \class  HeatCapacity
  *  \author Nate Yonkee
- *  \date May, 2014
+ *  \date July, 2014
  *
- *  \brief Calculates the constant pressure heat capacity of each species
+ *  \brief Calculates the constant pressure heat capacity of a mixture
  *  using either NASA7 or Shomate polynomials with 2 temperature ranges.
- *  Units of J/kmole/K
+ *  Units of J/kg/K
  *
  *  Five coefficients \f$(a_0,\dots,a_4)\f$ are used to represent
- * \f$ c_p^0(T)\f$ as a polynomial in \f$ T \f$
+ *  \f$ c_p^0(T)\f$ of species i as a polynomial in \f$ T \f$
  *
  * NASA7:
  *
  * \f[
- * \frac{c_p(T)}{R} = a_0 + a_1 T + a_2 T^2 + a_3 T^3 + a_4 T^4
+ * \frac{c_{p,i}(T)}{R} = a_0 + a_1 T + a_2 T^2 + a_3 T^3 + a_4 T^4
  * \f]
  *
  * Shomate:
  *
  * \f[
- * \frac{c_p(T)}{1000} = a_0 + a_1 t + a_2 t^2 + a_3 t^3 + \frac{a_4}{t^2}
+ * \frac{c_{p,i}(T)}{1000} = a_0 + a_1 t + a_2 t^2 + a_3 t^3 + \frac{a_4}{t^2}
  * \f]
  *
  * Where \f[ t = T (K)/1000 \f]
  *
+ * The heat capacity is a weighted average of the pure species \f$ c_p^0(T)\f$,
+ *
+ * \f[
+ * c_p(T) = \sum_{n=0}^{nSpec} Y_n c_{p,n}
+ * \f]
  */
+
 template< typename FieldT >
 class HeatCapacity_Cp
  : public Expr::Expression<FieldT>
@@ -50,29 +56,42 @@ class HeatCapacity_Cp
   typedef std::vector<FieldT*> SpecT;
   typedef std::vector<double>  SpecCoefs;
   const Expr::Tag tTag_;
+  const Expr::TagList tPowerTags_;
+  Expr::TagList massFracTags_;
   const FieldT* t_;
+  std::vector<const FieldT*> tPowers_;
+  std::vector<const FieldT*> massFracs_;
   Cantera_CXX::IdealGasMix* const gasMix_;
   const int nSpec_; // number of species
   bool nasaFlag_; // flag to specify if any NASA polynomials are present
   bool shomateFlag_; // flag to specify if any Shomate polynomials are present
 
-  HeatCapacity_Cp( const Expr::Tag& tTag );
+  HeatCapacity_Cp( const Expr::Tag& tTag,
+                   const Expr::TagList& tPowerTags,
+                   const Expr::Tag& massFracTag );
+
 public:
   class Builder : public Expr::ExpressionBuilder
   {
   public:
     /**
      *  @brief Build a HeatCapacity_Cp expression
-     *  @param resultTags the list of tags for each species' heat capacity, order must be consistent with Cantera's input file
+     *  @param resultTag the mixture heat capacity
      *  @param tTag tag for temperature
+     *  @param tPowerTags tags for powers of temperature for use in polynomial
+     *  @param massFracTag tag for mass fractions of each species, ordering is consistent with Cantera input
      */
-    Builder( const Expr::TagList& resultTags,
-             const Expr::Tag& tTag );
+    Builder( const Expr::Tag& resultTag,
+             const Expr::Tag& tTag,
+             const Expr::TagList& tPowerTags,
+             const Expr::Tag& massFracTag );
 
     Expr::ExpressionBase* build() const;
 
   private:
     const Expr::Tag tTag_;
+    const Expr::TagList tPowerTags_;
+    const Expr::Tag massFracTag_;
   };
 
   ~HeatCapacity_Cp();
@@ -93,26 +112,43 @@ public:
 
 template< typename FieldT >
 HeatCapacity_Cp<FieldT>::
-HeatCapacity_Cp( const Expr::Tag& tTag )
+HeatCapacity_Cp( const Expr::Tag& tTag,
+    const Expr::TagList& tPowerTags,
+    const Expr::Tag& massFracTag )
   : Expr::Expression<FieldT>(),
     tTag_( tTag ),
+    tPowerTags_( tPowerTags ),
     gasMix_( CanteraObjects::get_gasmix() ),
     nSpec_( gasMix_->nSpecies() ),
     nasaFlag_( false ),
     shomateFlag_( false )
 {
   this->set_gpu_runnable( true );
-  const Cantera::SpeciesThermo& spThermo = gasMix_->speciesThermo();
 
-  for(size_t n=0; n<nSpec_; ++n){
-    int type = spThermo.reportType(n);
-    if( type == NASA2 )
-      nasaFlag_ = true;
-    else if( type == SHOMATE2 )
-      shomateFlag_ = true;
-    else if( type != SIMPLE )
-      std::cout<<"Thermo type not supported,\nType = "<<type<<" species # "<<n<<std::endl;
+  massFracTags_.clear();
+  for( size_t n=0; n<nSpec_; ++n ){
+    std::ostringstream name;
+    name << massFracTag.name() << "_" << n;
+    massFracTags_.push_back( Expr::Tag(name.str(),massFracTag.context()) );
   }
+
+  const Cantera::SpeciesThermo& spThermo = gasMix_->speciesThermo();
+  for( size_t n=0; n<nSpec_; ++n ){
+    const int type = spThermo.reportType(n);
+    switch( type ){
+    case NASA2   : nasaFlag_    = true; break;
+    case SHOMATE2: shomateFlag_ = true; break;
+    case SIMPLE  :                      break;
+    default:{
+      std::ostringstream msg;
+      msg << __FILE__ << " : " << __LINE__
+          << "\nThermo type not supported,\n Type = " << type
+          << ", species # " << n << std::endl;
+      throw std::invalid_argument( msg.str() );
+    }
+    }
+  }
+
 }
 
 //--------------------------------------------------------------------
@@ -132,6 +168,8 @@ HeatCapacity_Cp<FieldT>::
 advertise_dependents( Expr::ExprDeps& exprDeps )
 {
   exprDeps.requires_expression( tTag_ );
+  exprDeps.requires_expression( tPowerTags_ );
+  exprDeps.requires_expression( massFracTags_ );
 }
 
 //--------------------------------------------------------------------
@@ -141,7 +179,10 @@ void
 HeatCapacity_Cp<FieldT>::
 bind_fields( const Expr::FieldManagerList& fml )
 {
-  t_ = &fml.template field_ref< FieldT >( tTag_ );
+  const typename Expr::FieldMgrSelector<FieldT>::type& fm = fml.field_manager<FieldT>();
+  t_ = &fm.field_ref( tTag_ );
+  for (size_t i=0; i<6; ++i) tPowers_.push_back(&fm.field_ref( tPowerTags_[i] ));
+  for (size_t n=0; n<nSpec_; ++n) massFracs_.push_back(&fm.field_ref( massFracTags_[n] ));
 }
 
 //--------------------------------------------------------------------
@@ -156,63 +197,50 @@ evaluate()
 #endif
   using namespace SpatialOps;
   using namespace Cantera;
-  SpecT& cps = this->get_value_vec();
+  FieldT& cp = this->value();
 
-  SpatFldPtr<FieldT> t2;
-  SpatFldPtr<FieldT> t3;
-  SpatFldPtr<FieldT> t4;
-  SpatFldPtr<FieldT> recipRecipT;
-
-  // pre-compute powers of t for polynomial evaluations
-    if( nasaFlag_ == true | shomateFlag_ == true){
-      t2 = SpatialFieldStore::get<FieldT>(*t_);
-      t3 = SpatialFieldStore::get<FieldT>(*t_);
-
-      *t2 <<= *t_ * *t_; // t^2
-      *t3 <<= *t2 * *t_; // t^3
-
-      if( nasaFlag_ == true ){
-        t4 = SpatialFieldStore::get<FieldT>(*t_);
-        *t4 <<= *t3 * *t_; // t^4
-      }
-
-      if( shomateFlag_ == true ){
-        recipRecipT = SpatialFieldStore::get<FieldT>(*t_);
-        *recipRecipT <<= 1/ *t2; // = t^-2
-      }
-    }
+  const FieldT& t2 = *tPowers_[0]; // t^2
+  const FieldT& t3 = *tPowers_[1]; // t^3
+  const FieldT& t4 = *tPowers_[2]; // t^4
+  const FieldT& recipRecipT = *tPowers_[5]; // t^-2
 
   std::vector<double> c(15,0); //vector of Cantera's coefficients
   int polyType;
-  double minTemp;
-  double maxTemp;
+  double minT;
+  double maxT;
   double refPressure;
   const Cantera::SpeciesThermo& spThermo = gasMix_->speciesThermo();
+  const std::vector<double>& molecularWeights = gasMix_->molecularWeights();
+
+  cp <<= 0.0; // set cp to 0 before starting summation
 
   for( size_t n=0; n<nSpec_; ++n ){
-    spThermo.reportParams(n, polyType, &c[0], minTemp, maxTemp, refPressure);
-    if (polyType==CONSTANT_CP | polyType==SIMPLE)
-      *cps[n] <<= c[3]; // constant heat capacity
-    /* polynomials are applicable in two temperature ranges - high and low
-     * If the temperature is out of range, the value is set to the value at the min or max temp
-     */
-    else if (polyType==NASA2){
+    spThermo.reportParams(n, polyType, &c[0], minT, maxT, refPressure);
+    switch (polyType) {
+    case SIMPLE:
+      cp <<= cp + *massFracs_[n] * c[3]
+                                 / molecularWeights[n];
+      break;
+    case NASA2:
+      for( std::vector<double>::iterator ic = c.begin() + 1; ic!=c.end(); ++ic)
+        *ic *= GasConstant / molecularWeights[n]; // dimensionalize coefficients
+      cp <<= cp + *massFracs_[n] * cond( *t_ <= c[0] && *t_ >= minT, c[1] + c[2] * *t_ + c[ 3] * t2 + c[ 4] * t3 + c[ 5] * t4 ) // if low temp
+                                       ( *t_ >  c[0] && *t_ <= maxT, c[8] + c[9] * *t_ + c[10] * t2 + c[11] * t3 + c[12] * t4 )  // else if high temp
+                                       ( *t_ < minT, c[1] + c[2] * minT + c[ 3] * minT * minT + c[ 4] * pow(minT,3) + c[ 5] * pow(minT,4) )  // else if out of bounds - low
+                                       (             c[8] + c[9] * maxT + c[10] * maxT * maxT + c[11] * pow(maxT,3) + c[12] * pow(maxT,4) ); // else out of bounds - high
+
+      break;
+    case SHOMATE2:
+      double minTScaled = minT/1000;
+      double maxTScaled = maxT/1000;
       for( std::vector<double>::iterator ic = c.begin() + 1; ic!=c.end(); ++ic )
-        *ic *= GasConstant; // dimensionalize the coefficients
-      *cps[n] <<= cond( *t_ <= c[0] && *t_ >= minTemp, c[1] + c[2] * *t_ + c[ 3] * *t2 + c[ 4] * *t3 + c[ 5] * *t4) // if low temp
-                      ( *t_ >  c[0] && *t_ <= maxTemp, c[8] + c[9] * *t_ + c[10] * *t2 + c[11] * *t3 + c[12] * *t4)  // else if high temp
-                      ( *t_ < minTemp, c[1] + c[2] * minTemp + c[ 3] * minTemp * minTemp + c[ 4] * pow(minTemp,3) + c[ 5] * pow(minTemp,4))  // else if out of bounds - low
-                      (                c[8] + c[9] * maxTemp + c[10] * maxTemp * maxTemp + c[11] * pow(maxTemp,3) + c[12] * pow(maxTemp,4)); // else out of bounds - high
-    }
-    else if (polyType==SHOMATE2){
-      for( std::vector<double>::iterator ic = c.begin() + 1; ic!=c.end(); ++ic )
-        *ic *= 1e3; // scale the coefficients
-      double minTempScaled = minTemp/1000;
-      double maxTempScaled = maxTemp/1000;
-      *cps[n] <<= cond( *t_ <= c[0] && *t_ >= minTemp, c[1] + c[2] * *t_*1e-3 + c[ 3] * *t2*1e-6 + c[ 4] * *t3*1e-9 + c[ 5] * *recipRecipT*1e6) // if low temp
-                      ( *t_ >  c[0] && *t_ <= maxTemp, c[8] + c[9] * *t_*1e-3 + c[10] * *t2*1e-6 + c[11] * *t3*1e-9 + c[12] * *recipRecipT*1e6)  // else if high temp
-                      ( *t_ < minTemp, c[1] + c[2] * minTempScaled + c[ 3] * minTempScaled * minTempScaled + c[ 4] * pow(minTempScaled,3) + c[ 5] * pow(minTempScaled,-2))  // else if out of bounds - low
-                      (                c[8] + c[9] * maxTempScaled + c[10] * maxTempScaled * maxTempScaled + c[11] * pow(maxTempScaled,3) + c[12] * pow(maxTempScaled,-2)); // else out of bounds - high
+        *ic *= 1e3 / molecularWeights[n]; // scale the coefficients to keep units consistent
+      cp <<= cp + *massFracs_[n] * cond( *t_ <= c[0] && *t_ >= minT, c[1] + c[2] * *t_*1e-3 + c[ 3] * t2*1e-6 + c[ 4] * t3*1e-9 + c[ 5] * recipRecipT*1e6 ) // if low temp
+                                       ( *t_ >  c[0] && *t_ <= maxT, c[8] + c[9] * *t_*1e-3 + c[10] * t2*1e-6 + c[11] * t3*1e-9 + c[12] * recipRecipT*1e6 )  // else if high temp
+                                       ( *t_ < minT, c[1] + c[2] * minTScaled + c[ 3] * minTScaled * minTScaled + c[ 4] * pow(minTScaled,3) + c[ 5] * pow(minTScaled,-2) )  // else if out of bounds - low
+                                       (             c[8] + c[9] * maxTScaled + c[10] * maxTScaled * maxTScaled + c[11] * pow(maxTScaled,3) + c[12] * pow(maxTScaled,-2) ); // else out of bounds - high
+
+      break;
     }
   }
 # ifdef TIMINGS
@@ -224,10 +252,14 @@ evaluate()
 
 template< typename FieldT >
 HeatCapacity_Cp<FieldT>::
-Builder::Builder( const Expr::TagList& resultTags,
-                  const Expr::Tag& tTag )
-  : ExpressionBuilder( resultTags ),
-    tTag_( tTag )
+Builder::Builder( const Expr::Tag& resultTag,
+                  const Expr::Tag& tTag,
+                  const Expr::TagList& tPowerTags,
+                  const Expr::Tag& massFracTag )
+: ExpressionBuilder( resultTag ),
+  tTag_( tTag ),
+  tPowerTags_( tPowerTags ),
+  massFracTag_( massFracTag )
 {}
 
 //--------------------------------------------------------------------
@@ -237,7 +269,7 @@ Expr::ExpressionBase*
 HeatCapacity_Cp<FieldT>::
 Builder::build() const
 {
-  return new HeatCapacity_Cp<FieldT>( tTag_ );
+  return new HeatCapacity_Cp<FieldT>( tTag_, tPowerTags_, massFracTag_ );
 }
 
 #endif // HeatCapacity_Cp_Expr_h
