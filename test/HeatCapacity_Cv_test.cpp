1bbb152f436a3a3455cc3a4976a8b74edb9d270c
Nathan Yonkee <nathan.yonkee@chemeng.utah.edu>
2014-08-25 17:13:49 -0600

    formatting

--- test/HeatCapacity_Cv_test.cpp
+++ test/HeatCapacity_Cv_test.cpp
@@ -41,95 +41,95 @@ int main()
   //const CanteraObjects::Setup setup( "Mix", "gri30.xml",         "gri30_mix" );
   //const CanteraObjects::Setup setup( "Mix", "ethanol_mech.xml",  "gas"       );
 
-  CanteraObjects::setup_cantera(setup);
-  Cantera_CXX::IdealGasMix* const gasMix = CanteraObjects::get_gasmix();
+    CanteraObjects::setup_cantera(setup);
+    Cantera_CXX::IdealGasMix* const gasMix = CanteraObjects::get_gasmix();
 
-  const int nSpec=gasMix->nSpecies();
-  size_t n;
-  const double refPressure=gasMix->pressure();
-  const std::vector<double>& molecularWeights = gasMix->molecularWeights();
+    const int nSpec=gasMix->nSpecies();
+    size_t n;
+    const double refPressure=gasMix->pressure();
+    const std::vector<double>& molecularWeights = gasMix->molecularWeights();
 
-  typedef Expr::PlaceHolder      < CellField > Temp;
-  typedef TemperaturePowers      < CellField > TemperaturePowers;
-  typedef Expr::PlaceHolder      < CellField > MassFracs;
-  typedef HeatCapacity_Cv        < CellField > HeatCapacity;
-  typedef SpeciesHeatCapacity_Cv < CellField > SpeciesHeatCapacity;
+    typedef Expr::PlaceHolder      < CellField > Temp;
+    typedef TemperaturePowers      < CellField > TemperaturePowers;
+    typedef Expr::PlaceHolder      < CellField > MassFracs;
+    typedef HeatCapacity_Cv        < CellField > HeatCapacity;
+    typedef SpeciesHeatCapacity_Cv < CellField > SpeciesHeatCapacity;
 
 
-  const Expr::Tag tTag ( "Temperature", Expr::STATE_NONE );
-  const Expr::Tag yiTag ( "yi", Expr::STATE_NONE );
-  Expr::TagList yiTags;
-  for( n=0; n<nSpec; ++n ){
-    std::ostringstream name;
-    name << "yi_" << n;
-    yiTags.push_back( Expr::Tag( name.str(), Expr::STATE_NONE ) );
-  }
-  Expr::Tag cvMixTag ("cv mix", Expr::STATE_NONE);
-  Expr::TagList cvTags;
-  for( n=0; n<nSpec; ++n ){
-    cvTags.push_back( Expr::Tag( "cv" + boost::lexical_cast<std::string>(n), Expr::STATE_NONE ) );
-  }
+    const Expr::Tag tTag ( "Temperature", Expr::STATE_NONE );
+    const Expr::Tag yiTag ( "yi", Expr::STATE_NONE );
+    Expr::TagList yiTags;
+    for( n=0; n<nSpec; ++n ){
+      std::ostringstream name;
+      name << "yi_" << n;
+      yiTags.push_back( Expr::Tag( name.str(), Expr::STATE_NONE ) );
+    }
+    Expr::Tag cvMixTag ("cv mix", Expr::STATE_NONE);
+    Expr::TagList cvTags;
+    for( n=0; n<nSpec; ++n ){
+      cvTags.push_back( Expr::Tag( "cv" + boost::lexical_cast<std::string>(n), Expr::STATE_NONE ) );
+    }
 
-  Expr::ExpressionFactory exprFactory;
+    Expr::ExpressionFactory exprFactory;
 
-  exprFactory.register_expression( new Temp::Builder(tTag) );
-  exprFactory.register_expression( new TemperaturePowers::Builder(tTag) );
-  BOOST_FOREACH( Expr::Tag yiTag, yiTags){
-    exprFactory.register_expression( new MassFracs::Builder (yiTag) );
-  }
-  const Expr::ExpressionID cvMixture_id = exprFactory.register_expression( new HeatCapacity::Builder( cvMixTag, tTag, yiTag ));
-  std::set< Expr::ExpressionID > cvSpecies_id;
-  for( n=0; n<nSpec; ++n ){
-    cvSpecies_id.insert(exprFactory.register_expression( new SpeciesHeatCapacity::Builder(cvTags[n], tTag, n) ));
-  }
+    exprFactory.register_expression( new Temp::Builder(tTag) );
+    exprFactory.register_expression( new TemperaturePowers::Builder(tTag) );
+    BOOST_FOREACH( Expr::Tag yiTag, yiTags){
+      exprFactory.register_expression( new MassFracs::Builder (yiTag) );
+    }
+    const Expr::ExpressionID cvMixture_id = exprFactory.register_expression( new HeatCapacity::Builder( cvMixTag, tTag, yiTag ));
+    std::set< Expr::ExpressionID > cvSpecies_id;
+    for( n=0; n<nSpec; ++n ){
+      cvSpecies_id.insert(exprFactory.register_expression( new SpeciesHeatCapacity::Builder(cvTags[n], tTag, n) ));
+    }
 
-  Expr::ExpressionTree mixtureTree( cvMixture_id, exprFactory, 0 );
-  Expr::ExpressionTree speciesTree( cvSpecies_id, exprFactory, 0 );
+    Expr::ExpressionTree mixtureTree( cvMixture_id, exprFactory, 0 );
+    Expr::ExpressionTree speciesTree( cvSpecies_id, exprFactory, 0 );
 
-  std::vector<int> ptvec;
+    std::vector<int> ptvec;
 #   ifdef TIMINGS
-  ptvec.push_back(8*8*8);
-  ptvec.push_back(16*16*16);
-  ptvec.push_back(32*32*32);
-  ptvec.push_back(64*64*64);
+    ptvec.push_back(8*8*8);
+    ptvec.push_back(16*16*16);
+    ptvec.push_back(32*32*32);
+    ptvec.push_back(64*64*64);
 #   ifdef ENABLE_CUDA
-  ptvec.push_back(128*128*128);
+    ptvec.push_back(128*128*128);
 #   endif
 #   else
-  ptvec.push_back(10);
+    ptvec.push_back(10);
 #   endif
 
-  for( std::vector<int>::iterator ptit = ptvec.begin(); ptit!= ptvec.end(); ++ptit){
-    size_t i;
+    for( std::vector<int>::iterator ptit = ptvec.begin(); ptit!= ptvec.end(); ++ptit){
+      size_t i;
 
-    So::IntVec npts(*ptit,1,1);
-    const So::BoundaryCellInfo cellBCInfo = So::BoundaryCellInfo::build<CellField>(false,false,false);
-    const So::GhostData cellGhosts(1);
-    const So::MemoryWindow vwindow( So::get_window_with_ghost(npts,cellGhosts,cellBCInfo) );
-    CellField xcoord( vwindow, cellBCInfo, cellGhosts, NULL );
+      So::IntVec npts(*ptit,1,1);
+      const So::BoundaryCellInfo cellBCInfo = So::BoundaryCellInfo::build<CellField>(false,false,false);
+      const So::GhostData cellGhosts(1);
+      const So::MemoryWindow vwindow( So::get_window_with_ghost(npts,cellGhosts,cellBCInfo) );
+      CellField xcoord( vwindow, cellBCInfo, cellGhosts, NULL );
 
-    std::vector<double> length(3,1.0);
-    So::Grid grid( npts, length );
-    grid.set_coord<SpatialOps::XDIR>( xcoord );
+      std::vector<double> length(3,1.0);
+      So::Grid grid( npts, length );
+      grid.set_coord<SpatialOps::XDIR>( xcoord );
 #     ifdef ENABLE_CUDA
-    xcoord.add_device( GPU_INDEX );
+      xcoord.add_device( GPU_INDEX );
 #     endif
 
-    Expr::FieldManagerList fml;
+      Expr::FieldManagerList fml;
 
-    mixtureTree.register_fields( fml );
-    speciesTree.register_fields( fml );
+      mixtureTree.register_fields( fml );
+      speciesTree.register_fields( fml );
 
-    fml.allocate_fields( Expr::FieldAllocInfo( npts, 0, 0, false, false, false ) );
+      fml.allocate_fields( Expr::FieldAllocInfo( npts, 0, 0, false, false, false ) );
 
-    mixtureTree.bind_fields( fml );
-    speciesTree.bind_fields( fml );
+      mixtureTree.bind_fields( fml );
+      speciesTree.bind_fields( fml );
 
-    using namespace SpatialOps;
-    Expr::FieldMgrSelector<CellField>::type& cellFM = fml.field_manager< CellField>();
+      using namespace SpatialOps;
+      Expr::FieldMgrSelector<CellField>::type& cellFM = fml.field_manager< CellField>();
 
-    CellField& temp = cellFM.field_ref(tTag);
-    temp <<= 500.0 + 1000.0*xcoord;
+      CellField& temp = cellFM.field_ref(tTag);
+      temp <<= 500.0 + 1000.0*xcoord;
 
       SpatFldPtr<CellField> sum  = SpatialFieldStore::get<CellField>(temp);
       *sum<<=0.0;
@@ -143,14 +143,14 @@ int main()
         yi <<= yi / *sum;
       }
 
-    mixtureTree.lock_fields( fml );  // prevent fields from being deallocated so that we can get them after graph execution.
-    speciesTree.lock_fields( fml );
+      mixtureTree.lock_fields( fml );  // prevent fields from being deallocated so that we can get them after graph execution.
+      speciesTree.lock_fields( fml );
 
 #     ifdef TIMINGS
-    std::cout << std::endl << setup.inputFile << " - " << *ptit << std::endl;
+      std::cout << std::endl << setup.inputFile << " - " << *ptit << std::endl;
 #     endif
-    boost::timer mixTimer;
-    mixtureTree.execute_tree();
+      boost::timer mixTimer;
+      mixtureTree.execute_tree();
 #     ifdef TIMINGS
       std::cout << "PoKiTT mixture cv time  " << mixTimer.elapsed() << std::endl;
 #     endif
