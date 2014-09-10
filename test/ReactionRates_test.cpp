02aa4e69be9b8c5d7bc74c8e1f2df74bdafbe4d3
Nathan Yonkee <nathan.yonkee@chemeng.utah.edu>
2014-09-09 18:46:03 -0600

    Rework reaction rates test and add tests for h2, gri, and ethanol mechanisms

--- test/ReactionRates_test.cpp
+++ test/ReactionRates_test.cpp
@@ -22,70 +22,121 @@
 #include <boost/lexical_cast.hpp>
 #include <boost/timer.hpp>
 #include <boost/foreach.hpp>
+#include <boost/program_options.hpp>
 
 #include <cantera/IdealGasMix.h>
 
 namespace Cantera_CXX{ class IdealGasMix; }
 
 namespace So = SpatialOps;
-typedef So::SVolField   CellField;
+typedef   So::SVolField   CellField;
 
-int main(){
-  try {
-    TestHelper status( true );
-    //const CanteraObjects::Setup setup( "Mix", "h2o2.xml",          "ohmech"    );
-    //const CanteraObjects::Setup setup( "Mix", "gri30.xml",         "gri30_mix" );
-    const CanteraObjects::Setup setup( "Mix", "ethanol_mech.xml",  "gas"       );
+namespace po = boost::program_options;
 
-    CanteraObjects::setup_cantera(setup);
-    Cantera_CXX::IdealGasMix* const gasMix = CanteraObjects::get_gasmix();
+//==============================================================================
 
-    const int nSpec=gasMix->nSpecies();
-    size_t n;
-    const double refPressure=gasMix->pressure();
-    const std::vector<double>& molecularWeights = gasMix->molecularWeights();
+const std::vector< So::SpatFldPtr<CellField> >
+get_cantera_results( const bool timings,
+                    Cantera_CXX::IdealGasMix& gasMix,
+                    const int nPts,
+                    CellField& temp){
+  using namespace SpatialOps;
 
-    typedef Expr::PlaceHolder <CellField> Temp;
-    typedef TemperaturePowers < CellField > TemperaturePowers;
-    typedef Expr::PlaceHolder <CellField> Pressure;
-    typedef Expr::PlaceHolder <CellField> MassFracs;
-    typedef MixtureMolWeight  <CellField> MixtureMolWeight;
-    typedef ReactionRates     <CellField> ReactionRate;
+  const double refPressure=gasMix.pressure();
+  const std::vector<double>& molecularWeights = gasMix.molecularWeights();
+  const int nSpec = gasMix.nSpecies();
 
-    const Expr::Tag tTag ( "Temperature"   , Expr::STATE_NONE);
-    const Expr::Tag pTag ( "Pressure"   , Expr::STATE_NONE);
-    const Expr::Tag mmwTag( "mmw", Expr::STATE_NONE);
-    const Expr::Tag yiTag ( "yi", Expr::STATE_NONE );
-    Expr::TagList yiTags;
-    for( n=0; n<nSpec; ++n ){
-      std::ostringstream name;
-      name << yiTag.name() << "_" << n;
-      yiTags.push_back( Expr::Tag(name.str(),yiTag.context()) );
+  std::vector< std::vector<double> > massfracs;
+  for( size_t i=0; i<nPts+2; ++i ){
+    std::vector<double> massfrac;
+    double sum = 0.0;
+    for( size_t n=0; n<nSpec; ++n){
+      massfrac.push_back(1 + n + (i-0.5)/ nPts);
+      sum+=massfrac[n];
     }
-    Expr::TagList rTags;
-    for( n=0; n<nSpec; ++n )
-      rTags.push_back( Expr::Tag( "ri" + boost::lexical_cast<std::string>(n), Expr::STATE_NONE ) );
+    for( size_t n=0; n<nSpec; ++n )
+      massfrac[n] = massfrac[n]/sum;
+    massfracs.push_back(massfrac);
+  }
 
-    Expr::ExpressionFactory exprFactory;
+  std::vector< SpatFldPtr<CellField> > canteraResults;
+  for( size_t n=0; n < nSpec; ++n){
+    canteraResults.push_back(SpatialFieldStore::get<CellField>(temp));
+  }
 
-    exprFactory.register_expression( new Temp ::Builder (tTag                 ) );
-    exprFactory.register_expression( new TemperaturePowers::Builder(tTag) );
-    exprFactory.register_expression( new Pressure ::Builder (pTag                 ) );
-    BOOST_FOREACH( Expr::Tag yiTag, yiTags){
-      exprFactory.register_expression( new MassFracs::Builder (yiTag) );
+  CellField::const_iterator iTemp = temp.begin();
+  std::vector< std::vector<double> >::iterator iMass = massfracs.begin();
+  std::vector<double> rResult(nSpec,0.0);
+
+  std::vector<double> r_result(nSpec,0.0);
+  boost::timer cTimer;
+  for( size_t i=0; i<nPts+2; ++iTemp, ++iMass, ++i){
+    gasMix.setState_TPY( *iTemp, refPressure, &(*iMass)[0]);
+    gasMix.getNetProductionRates(&rResult[0]);
+    for( size_t n=0; n<nSpec; ++n){
+      (*canteraResults[n])[i] = rResult[n];
     }
-    exprFactory.register_expression( new MixtureMolWeight::Builder( mmwTag, yiTag, molecularWeights));
-    Expr::ExpressionID rRate_id = exprFactory.register_expression( new ReactionRate::Builder (rTags, tTag, pTag, yiTag, mmwTag) );
+  }
+  if( timings ) std::cout << "Cantera reaction rate time " << cTimer.elapsed() << std::endl;
 
-    Expr::ExpressionTree tree( rRate_id, exprFactory, 0 );
+  for( size_t n=0; n<nSpec; ++n){
+    *canteraResults[n] <<= *canteraResults[n] * molecularWeights[n]; // convert to mass basis for field comparison
+  }
 
-    {
-      std::ofstream fout( "ReactionRate.dot" );
-      tree.write_tree(fout);
-    }
+  return canteraResults;
+}
 
-    std::vector<int> ptvec;
-#   ifdef TIMINGS
+bool driver( bool timings )
+{
+  TestHelper status( !timings );
+  Cantera_CXX::IdealGasMix* const gasMix = CanteraObjects::get_gasmix();
+
+  const int nSpec=gasMix->nSpecies();
+  size_t n;
+  const double refPressure=gasMix->pressure();
+  const std::vector<double>& molecularWeights = gasMix->molecularWeights();
+
+  typedef Expr::PlaceHolder <CellField> Temp;
+  typedef TemperaturePowers <CellField> TemperaturePowers;
+  typedef Expr::PlaceHolder <CellField> Pressure;
+  typedef Expr::PlaceHolder <CellField> MassFracs;
+  typedef MixtureMolWeight  <CellField> MixtureMolWeight;
+  typedef ReactionRates     <CellField> ReactionRate;
+
+  const Expr::Tag tTag  ( "Temperature", Expr::STATE_NONE);
+  const Expr::Tag pTag  ( "Pressure",    Expr::STATE_NONE);
+  const Expr::Tag mmwTag( "mmw",         Expr::STATE_NONE);
+  const Expr::Tag yiTag ( "yi",          Expr::STATE_NONE );
+  Expr::TagList yiTags;
+  for( n=0; n<nSpec; ++n ){
+    std::ostringstream name;
+    name << yiTag.name() << "_" << n;
+    yiTags.push_back( Expr::Tag(name.str(),yiTag.context()) );
+  }
+  Expr::TagList rTags;
+  for( n=0; n<nSpec; ++n )
+    rTags.push_back( Expr::Tag( "ri" + boost::lexical_cast<std::string>(n), Expr::STATE_NONE ) );
+
+  Expr::ExpressionFactory exprFactory;
+
+  exprFactory.register_expression( new Temp             ::Builder( tTag  ) );
+  exprFactory.register_expression( new TemperaturePowers::Builder( tTag  ) );
+  exprFactory.register_expression( new Pressure         ::Builder( pTag  ) );
+  BOOST_FOREACH( Expr::Tag yiTag, yiTags){
+    exprFactory.register_expression( new MassFracs      ::Builder( yiTag ) );
+  }
+  exprFactory.register_expression( new MixtureMolWeight ::Builder( mmwTag, yiTag, molecularWeights));
+  Expr::ExpressionID rRate_id = exprFactory.register_expression( new ReactionRate::Builder (rTags, tTag, pTag, yiTag, mmwTag) );
+
+  Expr::ExpressionTree tree( rRate_id, exprFactory, 0 );
+
+  {
+    std::ofstream fout( "ReactionRate.dot" );
+    tree.write_tree(fout);
+  }
+
+  std::vector<int> ptvec;
+  if( timings ){
     ptvec.push_back(8*8*8);
     ptvec.push_back(16*16*16);
     ptvec.push_back(32*32*32);
@@ -93,125 +144,145 @@ int main(){
 #   ifdef ENABLE_CUDA
     ptvec.push_back(128*128*128);
 #   endif
-#   else
+  }
+  else{
     ptvec.push_back(10);
+  }
+
+  for( std::vector<int>::iterator iPts = ptvec.begin(); iPts!= ptvec.end(); ++iPts){
+    So::IntVec npts(*iPts,1,1);
+    const So::BoundaryCellInfo cellBCInfo = So::BoundaryCellInfo::build<CellField>(false,false,false);
+    const So::GhostData cellGhosts(1);
+    const So::MemoryWindow vwindow( So::get_window_with_ghost(npts,cellGhosts,cellBCInfo) );
+    CellField xcoord( vwindow, cellBCInfo, cellGhosts, NULL );
+
+    std::vector<double> length(3,1.0);
+    So::Grid grid( npts, length );
+    grid.set_coord<SpatialOps::XDIR>( xcoord );
+#   ifdef ENABLE_CUDA
+    xcoord.add_device( GPU_INDEX );
 #   endif
 
-    for( std::vector<int>::iterator ptit = ptvec.begin(); ptit!= ptvec.end(); ++ptit){
-      size_t i;
+    Expr::FieldManagerList fml;
+    tree.register_fields( fml );
+    fml.allocate_fields( Expr::FieldAllocInfo( npts, 0, 0, false, false, false ) );
+    tree.bind_fields( fml );
 
-      So::IntVec npts(*ptit,1,1);
-      const So::BoundaryCellInfo cellBCInfo = So::BoundaryCellInfo::build<CellField>(false,false,false);
-      const So::GhostData cellGhosts(1);
-      const So::MemoryWindow vwindow( So::get_window_with_ghost(npts,cellGhosts,cellBCInfo) );
-      CellField xcoord( vwindow, cellBCInfo, cellGhosts, NULL );
+    using namespace SpatialOps;
+    Expr::FieldMgrSelector<CellField>::type& cellFM = fml.field_manager< CellField>();
 
-      std::vector<double> length(3,1.0);
-      So::Grid grid( npts, length );
-      grid.set_coord<SpatialOps::XDIR>( xcoord );
-#     ifdef ENABLE_CUDA
-      xcoord.add_device( GPU_INDEX );
-#     endif
+    CellField& temp = fml.field_manager<CellField>().field_ref(tTag);
+    temp <<= 500.0 + 1000 * xcoord;
 
-      Expr::FieldManagerList fml;
-      tree.register_fields( fml );
-      fml.allocate_fields( Expr::FieldAllocInfo( npts, 0, 0, false, false, false ) );
-      tree.bind_fields( fml );
+    CellField& p = cellFM.field_ref(pTag);
+    p <<= refPressure;
 
-      using namespace SpatialOps;
-      Expr::FieldMgrSelector<CellField>::type& cellFM = fml.field_manager< CellField>();
-
-      CellField& temp = fml.field_manager<CellField>().field_ref(tTag);
-      temp <<= 500.0 + 1000 * xcoord;
-
-      CellField& p = cellFM.field_ref(pTag);
-      p <<= refPressure;
-
-      SpatFldPtr<CellField> sum  = SpatialFieldStore::get<CellField>(temp);
-      *sum<<=0.0;
-      for( n=0; n<nSpec; ++n ){
-        CellField& yi = fml.field_manager<CellField>().field_ref(yiTags[n]);
-        yi <<= n + 1 + xcoord;
-        *sum <<= *sum + yi;
-      }
-      BOOST_FOREACH( Expr::Tag yiTag, yiTags){
-        CellField& yi = cellFM.field_ref(yiTag);
-        yi <<= yi / *sum;
-      }
-
-      tree.lock_fields(fml);
-
-      std::cout<<setup.inputFile<<" - "<<*ptit<<std::endl;
-      boost::timer treetimer;
-      tree.execute_tree();
-      std::cout << "tree time " << treetimer.elapsed() << std::endl;
-
-#     ifdef ENABLE_CUDA
-      for( n=0; n<nSpec; ++n){
-        CellField& r = fml.field_manager<CellField>().field_ref(rTags[n]);
-        r.add_device(CPU_INDEX);
-      }
-#     endif
-
-      std::vector<double> tVec;
-      for( i=0; i<*ptit+2; ++i)
-        tVec.push_back( 500.0 + 1000.0 * (i-0.5)/ *ptit);
-
-      std::vector< std::vector<double> > massfracs;
-      for( i=0; i<*ptit+2; ++i){
-        std::vector<double> massfrac;
-        double sum = 0.0;
-        for( n=0; n<nSpec; ++n){
-          massfrac.push_back(1 + n + (i-0.5)/ *ptit);
-          sum+=massfrac[n];
-        }
-        for( n=0; n<nSpec; ++n)
-          massfrac[n] = massfrac[n]/sum;
-        massfracs.push_back(massfrac);
-      }
-
-      std::vector< SpatFldPtr<CellField> > canteraResults;
-      for( n=0; n < nSpec; ++n){
-        canteraResults.push_back(SpatialFieldStore::get<CellField>(xcoord));
-#       ifdef ENABLE_CUDA
-        canteraResults[n]->add_device( CPU_INDEX );
-#       endif
-      }
-      i=0;
-      std::vector<double>::const_iterator itemp = tVec.begin();
-      std::vector< std::vector<double> >::iterator imass = massfracs.begin();
-      std::vector<double> r_result(nSpec,0.0);
-      boost::timer cTimer;
-      for( i=0; i<*ptit+2; ++itemp, ++imass, ++i){
-        gasMix->setState_TPY( *itemp, refPressure, &(*imass)[0]);
-        gasMix->getNetProductionRates(&r_result[0]);
-        for( n=0; n<nSpec; ++n){
-          (*canteraResults[n])[i] = r_result[n];
-        }
-      }
-
-      for( n=0; n<nSpec; ++n){
-        *canteraResults[n] <<= *canteraResults[n] * molecularWeights[n];
-        CellField& r = cellFM.field_ref(rTags[n]);
-        status( field_equal(r, *canteraResults[n], 1e-10, 1e-8), n);
-      }
-
-    } // number of points
-
-    if( status.ok() ){
-      std::cout << "PASS\n";
-      return 0;
+    SpatFldPtr<CellField> sum  = SpatialFieldStore::get<CellField>(temp);
+    *sum<<=0.0;
+    for( n=0; n<nSpec; ++n ){
+      CellField& yi = fml.field_manager<CellField>().field_ref(yiTags[n]);
+      yi <<= n + 1 + xcoord;
+      *sum <<= *sum + yi;
+    }
+    BOOST_FOREACH( Expr::Tag yiTag, yiTags){
+      CellField& yi = cellFM.field_ref(yiTag);
+      yi <<= yi / *sum;
     }
 
-  } // try
+    tree.lock_fields(fml);
+
+    if( timings ) std::cout << std::endl << "Reaction rates test - " << *iPts << std::endl;
+
+    boost::timer rxnTimer;
+    tree.execute_tree();
+
+    if( timings ) std::cout << "PoKiTT time " << rxnTimer.elapsed() << std::endl;
+
+#   ifdef ENABLE_CUDA
+    BOOST_FOREACH( Expr::Tag rTag, rTags){
+      CellField& r = fml.field_manager<CellField>().field_ref(rTag);
+      r.add_device(CPU_INDEX);
+    }
+#   endif
+
+    const std::vector< SpatFldPtr<CellField> > canteraResults = get_cantera_results( timings,
+                                                                                     *gasMix,
+                                                                                     *iPts,
+                                                                                     temp );
+
+    std::vector< SpatFldPtr<CellField> >::const_iterator iCantera = canteraResults.begin();
+    BOOST_FOREACH( const Expr::Tag& rTag, rTags ){
+      CellField& r = cellFM.field_ref(rTag);
+      status( field_equal( r, **iCantera, 1e-8 ) || field_equal_abs( r, **iCantera, 1e-10 ), rTag.name() );
+      ++iCantera;
+    }
+
+  } // number of points
+
+  return status.ok();
+}
+
+//==============================================================================
+
+int main( int iarg, char* carg[] )
+{
+  std::string inputFileName;
+  std::string inpGroup;
+  bool timings = false;
+
+  // parse the command line options input describing the problem
+  try {
+    po::options_description desc("Supported Options");
+    desc.add_options()
+           ( "help", "print help message" )
+           ( "xml-input-file", po::value<std::string>(&inputFileName), "Cantera xml input file name" )
+           ( "phase", po::value<std::string>(&inpGroup), "name of phase in Cantera xml input file" )
+           ( "timings", "Generate comparison timings between Cantera and PoKiTT across several problem sizes" );
+
+    po::variables_map args;
+    po::store( po::parse_command_line(iarg,carg,desc), args );
+    po::notify(args);
+
+    timings = args.count("timings") > 0;
+
+    if (!args.count("xml-input-file")){
+      std::cout << "You must enter an xml input file for Cantera" << std::endl;
+      std::cout << desc << std::endl;
+      return 1;
+    }
+
+    if (args.count("help")) {
+      std::cout << desc << "\n";
+      return -1;
+    }
+  }
+
+  catch( std::exception& err ){
+    std::cout << "Error parsing input arguments\n" << err.what() << std::endl;
+    return -2;
+  }
+
+  try{
+    const CanteraObjects::Setup setup( "Mix", inputFileName, inpGroup );
+    CanteraObjects::setup_cantera( setup );
+
+    TestHelper status( !timings );
+    status( driver( timings ), "Reaction Rates" );
+
+    if( status.ok() ){
+      std::cout << "\nPASS\n";
+      return 0;
+    }
+  }
   catch( Cantera::CanteraError& ){
     Cantera::showErrors();
   }
-
   catch( std::exception& err ){
     std::cout << err.what() << std::endl;
   }
 
-  std::cout << "FAIL\n";
+  std::cout << "\nFAIL\n";
   return -1;
 }
+
+//==============================================================================
