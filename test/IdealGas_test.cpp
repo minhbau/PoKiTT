44c53f5f314afc8a9ae47d1fb1286b9543e39d4d
Nathan Yonkee <nathan.yonkee@chemeng.utah.edu>
2015-01-30 17:35:09 -0700

    Use ExprLibrary interfaces instead of SpatialOps whenever possible

--- test/IdealGas_test.cpp
+++ test/IdealGas_test.cpp
@@ -7,7 +7,7 @@
 
 #include <iostream>
 #include "TestHelper.h"
-
+#include "LinearMassFracs.h"
 #include <pokitt/thermo/Density.h>
 #include <pokitt/thermo/Pressure.h>
 #include <pokitt/MixtureMolWeight.h>
@@ -25,7 +25,7 @@
 #include <cantera/IdealGasMix.h>
 
 namespace So = SpatialOps;
-typedef So::SVolField   CellField;
+typedef So::SVolField  CellField;
 typedef So::SpatFldPtr<CellField> CellFieldPtrT;
 
 namespace Cantera_CXX{ class IdealGasMix; }
@@ -54,38 +54,25 @@ std::string property_name( const GasQuantity q )
 //==============================================================================
 
 const std::vector< std::vector<double> >
-calculate_mass_fracs(const int nSpec, CellField& xcoord, const Expr::TagList yiTags, Expr::FieldManagerList& fml){
-  Expr::FieldMgrSelector<CellField>::type& cellFM = fml.field_manager<CellField>();
-  CellField& yi = cellFM.field_ref(yiTags[0]);
-  CellFieldPtrT sum = So::SpatialFieldStore::get<CellField>(yi);
-  *sum<<=0.0;
-  for( size_t n=0; n<nSpec; ++n ){
-    CellField& yi = cellFM.field_ref(yiTags[n]);
-    yi <<= n + 1 + xcoord;
-    *sum <<= *sum + yi;
-  }
-  BOOST_FOREACH( Expr::Tag yiTag, yiTags){
-    CellField& yi = cellFM.field_ref(yiTag);
-    yi <<= yi / *sum;
-  }
+extract_mass_fracs( const Expr::TagList yiTags, Expr::FieldManagerList& fml ){
+  CellField& yi = fml.field_ref< CellField >(yiTags[0]);
+  const size_t nPts = yi.window_with_ghost().glob_npts();
+  const int nSpec = yiTags.size();
+
   std::vector< std::vector<double> > massFracs;
-  size_t nPts = xcoord.window_with_ghost().glob_npts();
   for( size_t i=0; i<nPts; ++i ){
     std::vector<double> massFrac( nSpec, 0.0 );
     massFracs.push_back(massFrac);
   }
   for( size_t n=0; n<nSpec; ++n ){
-    CellField& yi = cellFM.field_ref(yiTags[n]);
+    yi = fml.field_ref< CellField >( yiTags[n] );
 #   ifdef ENABLE_CUDA
     yi.set_device_as_active( CPU_INDEX );
 #   endif
     size_t i=0;
-    for( CellField::iterator iY = yi.begin(); iY != yi.end(); ++iY, ++i ){
+    for( CellField::const_iterator iY = yi.begin(); iY != yi.end(); ++iY, ++i ){
       massFracs[i][n] = *iY;
     }
-#   ifdef ENABLE_CUDA
-    yi.set_device_as_active( GPU_INDEX );
-#   endif
   }
   return massFracs;
 }
@@ -97,10 +84,14 @@ get_cantera_result( const bool timings,
                     const size_t canteraReps,
                     const GasQuantity gasQuantity,
                     Cantera_CXX::IdealGasMix& gasMix,
-                    const std::vector< std::vector<double> >& massFracs,
-                    CellField& temp,
-                    CellField& refQuantity)
+                    Expr::FieldManagerList& fml,
+                    const Expr::TagList& yiTags,
+                    const Expr::Tag& tTag,
+                    const Expr::Tag& refTag )
 {
+  CellField& temp = fml.field_ref< CellField >( tTag );
+  CellField& refQuantity = fml.field_ref< CellField >( refTag );
+  const std::vector< std::vector<double> > massFracs = extract_mass_fracs( yiTags, fml );
 # ifdef ENABLE_CUDA
   temp.set_device_as_active       ( CPU_INDEX );
   refQuantity.set_device_as_active( CPU_INDEX );
@@ -148,104 +139,81 @@ bool driver( const bool timings,
 
   const int nSpec = gasMix->nSpecies();
 
-  typedef Expr::PlaceHolder <CellField> Temperature;
-  typedef Expr::PlaceHolder <CellField> RefQuantity;
-  typedef Expr::PlaceHolder <CellField> MassFracs;
-  typedef MixtureMolWeight  <CellField> MixtureMolWeight;
+  typedef Expr::PlaceHolder      <CellField> XCoord;
+  typedef Expr::LinearFunction   <CellField> Temperature;
+  typedef Expr::ConstantExpr     <CellField> RefQuantity;
+  typedef       LinearMassFracs  <CellField> MassFracs;
+  typedef       MixtureMolWeight <CellField> MixtureMolWeight;
 
-  const Expr::Tag tTag(   "Temperature",        Expr::STATE_NONE );
-  Expr::Tag refTag;
-  switch( gasQuantity ){
-  case P:
-    refTag = Expr::Tag( "Density",  Expr::STATE_NONE ); break;
-  case RHO:
-    refTag = Expr::Tag( "Pressure", Expr::STATE_NONE ); break;
-  }
+  const Expr::Tag xTag( "XCoord",      Expr::STATE_NONE );
+  const Expr::Tag tTag( "Temperature", Expr::STATE_NONE );
   Expr::TagList yiTags;
   for( size_t n=0; n<nSpec; ++n ){
     yiTags.push_back( Expr::Tag( "yi_" + boost::lexical_cast<std::string>(n), Expr::STATE_NONE ) );
   }
   const Expr::Tag mmwTag ("Mixture Molecular Weight", Expr::STATE_NONE);
+  Expr::Tag refTag;
+  switch( gasQuantity ){
+  case P:   refTag = Expr::Tag( "Density",  Expr::STATE_NONE ); break;
+  case RHO: refTag = Expr::Tag( "Pressure", Expr::STATE_NONE ); break;
+  }
   const Expr::Tag gasTag (property_name(gasQuantity), Expr::STATE_NONE);
 
   Expr::ExpressionFactory exprFactory;
 
-  BOOST_FOREACH( const Expr::Tag& yiTag, yiTags){
-    exprFactory.register_expression( new MassFracs::Builder( yiTag ) );
-  }
-  exprFactory.register_expression( new Temperature::Builder     ( tTag           ) );
-  exprFactory.register_expression( new RefQuantity::Builder     ( refTag         ) );
+  exprFactory.register_expression( new XCoord::Builder          ( xTag           ) );
+  exprFactory.register_expression( new MassFracs::Builder       ( yiTags, xTag   ) );
+  exprFactory.register_expression( new Temperature::Builder     ( tTag,   xTag, 1000, 500 ) );
   exprFactory.register_expression( new MixtureMolWeight::Builder( mmwTag, yiTags ) );
 
   Expr::ExpressionID gas_id;
   switch( gasQuantity ){
   case P:
-    gas_id = exprFactory.register_expression( new Pressure<CellField>::Builder(gasTag, tTag, refTag, mmwTag) );
+    gas_id = exprFactory.register_expression( new Pressure<CellField>::Builder( gasTag, tTag, refTag, mmwTag) );
+    exprFactory.register_expression(          new RefQuantity        ::Builder( refTag, gasMix->density()   ) );
     break;
   case RHO:
     gas_id = exprFactory.register_expression( new Density<CellField>::Builder (gasTag, tTag, refTag, mmwTag) );
+    exprFactory.register_expression(          new        RefQuantity::Builder ( refTag, gasMix->pressure() ) );
     break;
   }
 
-  std::vector<So::IntVec> ptvec;
+  Expr::ExpressionTree gasTree( gas_id , exprFactory, 0 );
+  {
+    std::ofstream gasGraph( "IdealGas.dot" );
+    gasTree.write_tree( gasGraph );
+  }
+  Expr::FieldManagerList fml;
+  gasTree.register_fields( fml );
+  gasTree.bind_fields( fml );
+  gasTree.lock_fields( fml );
+
+  std::vector<So::IntVec> sizeVec;
   if( timings ){
-    ptvec.push_back( So::IntVec(126,126,126) );
-    ptvec.push_back( So::IntVec( 62, 62, 62) );
-    ptvec.push_back( So::IntVec( 30, 30, 30) );
-    ptvec.push_back( So::IntVec( 14, 14, 14) );
-    ptvec.push_back( So::IntVec(  6,  6,  6) );
+    sizeVec.push_back( So::IntVec(126,126,126) );
+    sizeVec.push_back( So::IntVec( 62, 62, 62) );
+    sizeVec.push_back( So::IntVec( 30, 30, 30) );
+    sizeVec.push_back( So::IntVec( 14, 14, 14) );
+    sizeVec.push_back( So::IntVec(  6,  6,  6) );
   }
   else{
-    ptvec.push_back( So::IntVec(20,1,1) );
+    sizeVec.push_back( So::IntVec(20,1,1) );
   }
 
-  for( std::vector<So::IntVec>::iterator iPts = ptvec.begin(); iPts!= ptvec.end(); ++iPts){
+  for( std::vector<So::IntVec>::iterator iSize = sizeVec.begin(); iSize!= sizeVec.end(); ++iSize){
 
-    Expr::ExpressionTree gasTree( gas_id , exprFactory, 0 );
-    {
-      std::ofstream gasGraph( "IdealGas.dot" );
-      gasTree.write_tree( gasGraph );
-    }
+    So::IntVec gridSize = *iSize;
+    fml.allocate_fields( Expr::FieldAllocInfo( gridSize, 0, 0, false, false, false ) );
+    So::Grid grid( gridSize, So::DoubleVec(1,1,1) );
 
-    So::IntVec nPts = *iPts;
-    const So::BoundaryCellInfo cellBCInfo = So::BoundaryCellInfo::build<CellField>(false,false,false);
-    const So::GhostData cellGhosts(1);
-    const So::MemoryWindow vwindow( So::get_window_with_ghost(nPts,cellGhosts,cellBCInfo) );
-    CellField xcoord( vwindow, cellBCInfo, cellGhosts, NULL );
-
-    std::vector<double> length(3,1.0);
-    So::Grid grid( nPts, length );
-    grid.set_coord<SpatialOps::XDIR>( xcoord );
+    CellField& xcoord = fml.field_ref< CellField >( xTag );
+    grid.set_coord<So::XDIR>( xcoord );
+    const int nPoints = xcoord.window_with_ghost().glob_npts();
 #   ifdef ENABLE_CUDA
-    xcoord.add_device( GPU_INDEX );
+    xcoord.set_device_as_active( GPU_INDEX );
 #   endif
 
-    Expr::FieldManagerList fml;
-
-    gasTree.register_fields( fml );
-    fml.allocate_fields( Expr::FieldAllocInfo( nPts, 0, 0, false, false, false ) );
-    gasTree.bind_fields( fml );
-
-    using namespace SpatialOps;
-    Expr::FieldMgrSelector<CellField>::type& cellFM = fml.field_manager< CellField>();
-
-    CellField& temp        = cellFM.field_ref( tTag   );
-    CellField& refQuantity = cellFM.field_ref( refTag );
-    CellField& mixMW       = cellFM.field_ref( mmwTag );
-    CellField& gasField    = cellFM.field_ref( gasTag );
-
-    temp <<= 500.0 + 1000 * xcoord;
-
-    switch( gasQuantity ){
-      case P:   refQuantity <<= gasMix->density();  break;
-      case RHO: refQuantity <<= gasMix->pressure(); break;
-    }
-
-    const std::vector< std::vector<double> > massFracs = calculate_mass_fracs( nSpec, xcoord, yiTags, fml );
-
-    gasTree.lock_fields( fml );  // prevent fields from being deallocated so that we can get them after graph execution.
-
-    if( timings ) std::cout << std::endl << property_name(gasQuantity) + " test - " << vwindow.glob_npts() << std::endl;
+    if( timings ) std::cout << std::endl << property_name(gasQuantity) + " test - " << nPoints << std::endl;
 
     Timer gasTimer;
     gasTimer.start();
@@ -256,13 +224,15 @@ bool driver( const bool timings,
 
     if( timings ) std::cout << "PoKiTT  " + property_name(gasQuantity) + " time " << gasTimer.elapsed_time()/pokittReps << std::endl;
 
+    CellFieldPtrT canteraResult = get_cantera_result( timings, canteraReps, gasQuantity, *gasMix, fml, yiTags, tTag, refTag );
+    CellField& gasField    = fml.field_ref< CellField >( gasTag );
 #   ifdef ENABLE_CUDA
     gasField.set_device_as_active( CPU_INDEX );
 #   endif
 
-    CellFieldPtrT canteraResult = get_cantera_result( timings, canteraReps, gasQuantity, *gasMix, massFracs, temp, refQuantity);
-
     status( field_equal(gasField, *canteraResult, 1e-12), gasTag.name() );
+
+    fml.deallocate_fields();
 
   }
   return status.ok();
---
+++ test/LinearMassFracs.h
@@ -0,0 +1,139 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2015 The University of Utah
+ *
+  * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+#ifndef LinearMassFracs_Expr_h
+#define LinearMassFracs_Expr_h
+
+#include <expression/Expression.h>
+
+/**
+ *  \class LinearMassFracs
+ */
+template< typename FieldT >
+class LinearMassFracs
+ : public Expr::Expression<FieldT>
+{
+  DECLARE_FIELD( FieldT, xCoord_ )
+  const int nSpec_;
+
+  /* declare any operators associated with this expression here */
+  
+  LinearMassFracs( const Expr::Tag& xCoordTag,
+                   const int nSpec );
+
+public:
+
+  class Builder : public Expr::ExpressionBuilder
+  {
+  public:
+    /**
+     *  @brief Build a LinearMassFracs expression
+     *  @param resultTag the tag for the value that this expression computes
+     *  @param xCoordTag independent variable for linear function
+     */
+    Builder( const Expr::TagList& resultTags,
+             const Expr::Tag& xCoordTag,
+             const int nghost = DEFAULT_NUMBER_OF_GHOSTS );
+
+    Expr::ExpressionBase* build() const;
+
+  private:
+    const Expr::Tag xCoordTag_;
+    const int nSpec_;
+  };
+
+  ~LinearMassFracs(){}
+  void evaluate();
+};
+
+
+
+// ###################################################################
+//
+//                          Implementation
+//
+// ###################################################################
+
+
+
+template< typename FieldT >
+LinearMassFracs<FieldT>::
+LinearMassFracs( const Expr::Tag& xCoordTag,
+                 const int nSpec )
+  : Expr::Expression<FieldT>(),
+    nSpec_( nSpec )
+{
+  this->set_gpu_runnable( true );
+  xCoord_ = this->template create_field_request<FieldT>( xCoordTag );
+}
+
+//--------------------------------------------------------------------
+
+template< typename FieldT >
+void
+LinearMassFracs<FieldT>::
+evaluate()
+{
+  std::vector< FieldT* >& massFracs = this->get_value_vec();
+
+  const FieldT& xCoord = xCoord_->field_ref();
+  SpatialOps::SpatFldPtr<FieldT> sum = SpatialOps::SpatialFieldStore::get<FieldT>(xCoord);
+
+  *sum<<=0.0;
+  for( size_t n=0; n<nSpec_; ++n ){
+    FieldT& yi = *massFracs[n];
+    yi <<= n + 1 + xCoord;
+    *sum <<= *sum + yi;
+  }
+  for( size_t n=0; n<nSpec_; ++n ){
+    FieldT& yi = *massFracs[n];
+    yi <<= yi / *sum;
+  }
+
+}
+
+//--------------------------------------------------------------------
+
+template< typename FieldT >
+LinearMassFracs<FieldT>::
+Builder::Builder( const Expr::TagList& resultTags,
+                  const Expr::Tag& xCoordTag,
+                  const int nghost )
+  : ExpressionBuilder( resultTags, nghost ),
+    xCoordTag_( xCoordTag ),
+    nSpec_( resultTags.size() )
+{}
+
+//--------------------------------------------------------------------
+
+template< typename FieldT >
+Expr::ExpressionBase*
+LinearMassFracs<FieldT>::
+Builder::build() const
+{
+  return new LinearMassFracs<FieldT>( xCoordTag_, nSpec_ );
+}
+
+
+#endif // LinearMassFracs_Expr_h
