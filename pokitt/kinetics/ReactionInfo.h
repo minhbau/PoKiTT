a3c41e4b20fc4f4e6600f1942dc367ad2be90b81
James C. Sutherland <James.Sutherland@utah.edu>
2016-11-14 13:53:39 -0700

    Avoid multiple symbol definition by moving a constructor to a .cpp file.

--- pokitt/kinetics/ReactionInfo.h
+++ pokitt/kinetics/ReactionInfo.h
@@ -31,12 +31,13 @@
 #ifndef REACTIONINFO_H_
 #define REACTIONINFO_H_
 
+#include <pokitt/CanteraObjects.h>
 
 namespace pokitt
 {
 
   // the functional form for rate constant k
-  // if the activation energy is 0 and "b" is an integer, we can avoid evaluating an exponentional
+  // if the activation energy is 0 and "b" is an integer, we can avoid evaluating an exponential
   enum RateConstantForm{
     CONSTANT,
     LINEAR,
@@ -80,109 +81,6 @@ namespace pokitt
     int sumProductStoich;
   };
 
-  ReactionInfo::ReactionInfo( const RxnData& dat )
-  {
-    const SpecDataVecT& rxts = dat.reactants;
-    switch( rxts.size() ){
-      case 1:
-        if(      rxts[0].stoich == 1 ) forwardOrder = ONE;
-        else if( rxts[0].stoich == 2 ) forwardOrder = TWO;
-        else                           forwardOrder = OTHER;
-        break;
-      case 2:
-        if(      rxts[0].stoich == 1 && rxts[1].stoich == 1 ) forwardOrder = ONE_ONE;
-        else if( rxts[0].stoich == 1 && rxts[1].stoich == 2 ) forwardOrder = ONE_TWO;
-        else if( rxts[0].stoich == 2 && rxts[1].stoich == 1 ) forwardOrder = TWO_ONE;
-        else                                                  forwardOrder = OTHER;
-        break;
-      case 3:
-        if( rxts[0].stoich == 1 && rxts[1].stoich == 1 && rxts[2].stoich == 1 ) forwardOrder = ONE_ONE_ONE;
-        else                                                                    forwardOrder = OTHER;
-        break;
-      default:
-        forwardOrder = OTHER;
-    }
-    sumReactantStoich = 0;
-    for( size_t s=0; s<rxts.size(); ++s ){
-      sumReactantStoich += std::abs( rxts[s].stoich );
-    }
-
-    const SpecDataVecT& prods = dat.products;
-    switch( prods.size() ){
-      case 1:
-        if(      prods[0].stoich == -1 ) reverseOrder = ONE;
-        else if( prods[0].stoich == -2 ) reverseOrder = TWO;
-        else                             reverseOrder = OTHER;
-        break;
-      case 2:
-        if(      prods[0].stoich == -1 && prods[1].stoich == -1 ) reverseOrder = ONE_ONE;
-        else if( prods[0].stoich == -1 && prods[1].stoich == -2 ) reverseOrder = ONE_TWO;
-        else if( prods[0].stoich == -2 && prods[1].stoich == -1 ) reverseOrder = TWO_ONE;
-        else                                                      reverseOrder = OTHER;
-        break;
-      case 3:
-        if( prods[0].stoich == -1 && prods[1].stoich == -1 && prods[2].stoich == -1 ) reverseOrder = ONE_ONE_ONE;
-        else                                                                          reverseOrder = OTHER;
-        break;
-      default:
-        reverseOrder = OTHER;
-    }
-    sumProductStoich = 0;
-    for( size_t s=0; s<prods.size(); ++s ){
-      sumProductStoich += std::abs( prods[s].stoich );
-    }
-
-    /*
-     * Here we are checking if the rate constant is Arrhenius in form
-     * If it is, we need to perform an exponential evaluation (expensive)
-     * If not, then we determine which power of temperature to use
-     */
-    if( fabs( dat.kFwdCoefs[2] ) < 1e-6 ){ // i.e. 0 activation energy
-      if(      fabs( dat.kFwdCoefs[1]     ) < 1e-6 ) kForm = CONSTANT;   // i.e. b=0
-      else if( fabs( dat.kFwdCoefs[1] - 1 ) < 1e-6 ) kForm = LINEAR;     // i.e. b=1
-      else if( fabs( dat.kFwdCoefs[1] - 2 ) < 1e-6 ) kForm = QUADRATIC;  // i.e. b=2
-      else if( fabs( dat.kFwdCoefs[1] + 1 ) < 1e-6 ) kForm = RECIPROCAL; // i.e. b=-1
-      else kForm = ARRHENIUS; // activation energy but non-integer value for b
-    }
-    else kForm = ARRHENIUS;
-
-    // now we check which terms of the troe function are negligible
-    const double* troe = dat.troeParams;
-    switch( dat.type ){
-      case ELEMENTARY: troeForm = NONE; break;
-      case THIRD_BODY: troeForm = NONE; break;
-      case LINDEMANN:  troeForm = NONE; break;
-      case TROE:
-        troeForm = NONE;
-        if( std::abs(troe[1]) > 1e-8 ){  // 1 is present
-          if( std::abs(troe[2]) > 1e-8 ){ // 1 and 2 are present
-            if( std::abs(troe[3]) > 1e-8 )    troeForm = T123;
-            else                              troeForm = T12;
-          }
-          else if( std::abs(troe[3]) > 1e-8 ) troeForm = T13;
-          else                                troeForm = T1;
-        }
-        else{ // 1 is not present
-          if( std::abs(troe[2]) > 1e-8 ){ // 1 not present, 2 is present
-            if( std::abs(troe[3]) > 1e-8 )    troeForm = T23;
-            else                              troeForm = T2;
-          }
-          else if( std::abs(troe[3]) > 1e-8 ) troeForm = T3;
-        }
-        break;
-      default: {
-        std::ostringstream msg;
-        msg << __FILE__ << " : " << __LINE__
-            << "\n Unknown reaction type somehow evaded detection\n"
-            << "This should have been caught during construction of CanteraObjects\n"
-            << "Check your xml input file to ensure it is not corrupted\n";
-        throw std::runtime_error( msg.str() );
-      }
-    } // switch( dat.type )
-  }
-
 }
-
-
 
 #endif /* REACTIONINFO_H_ */
